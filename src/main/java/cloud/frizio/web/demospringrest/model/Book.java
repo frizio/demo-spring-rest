deaa601ff76e0826b40a222b03c007434a52b1b6
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-18 13:31:35 +0200

    Add Book model class

---
+++ src/main/java/cloud/frizio/web/demospringrest/model/Book.java
@@ -0,0 +1,26 @@
+package cloud.frizio.web.demospringrest.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String name;
+    
+    private String author;
+    
+    private BigDecimal price;
+    
+}
