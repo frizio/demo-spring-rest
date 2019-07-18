483af415abf7e4e26a6a5af418cee05614787cda
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-18 15:12:47 +0200

    Update a book author (PATCH method)

--- src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
+++ src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
@@ -1,10 +1,13 @@
 package cloud.frizio.web.demospringrest.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import cloud.frizio.web.demospringrest.error.BookNotFoundException;
+import cloud.frizio.web.demospringrest.error.BookUnSupportedFieldPatchException;
 import cloud.frizio.web.demospringrest.model.Book;
 import cloud.frizio.web.demospringrest.repository.BookRepository;;
 
@@ -59,6 +63,25 @@ public class BookController {
                     newBook.setId(id);
                     return repository.save(newBook);
                 });
+    }
+
+    // Update author only
+    @PatchMapping("/books/{id}")
+    Book patch(@RequestBody Map<String, String> update, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(x -> {
+                    String author = update.get("author");
+                    if (!StringUtils.isEmpty(author)) {
+                        x.setAuthor(author);
+                        return repository.save(x);
+                    } else {
+                        throw new BookUnSupportedFieldPatchException(update.keySet());
+                    }
+                })
+                .orElseGet(() -> {
+                    throw new BookNotFoundException(id);
+                });
+
     }
 
 }
