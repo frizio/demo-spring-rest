e38fd457e4d9f7e1da1c79f915a9da8ff724f5bc
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-18 14:50:47 +0200

    Read a book by id (GET method)

--- src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
+++ src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import cloud.frizio.web.demospringrest.error.BookNotFoundException;
 import cloud.frizio.web.demospringrest.model.Book;
 import cloud.frizio.web.demospringrest.repository.BookRepository;;
 
@@ -33,6 +35,13 @@ public class BookController {
     @PostMapping("/books")
     Book newBook(@RequestBody Book newBook) {
         return repository.save(newBook);
+    }
+
+    // Find
+    @GetMapping("/books/{id}")
+    Book findOne(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
     }
     
 }
