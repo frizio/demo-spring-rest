00b0923a3ff559cf739335be3a52f0ba8a047176
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-18 14:01:13 +0200

    List all books (GET mapping method)

--- src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
+++ src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
@@ -1,8 +1,12 @@
 package cloud.frizio.web.demospringrest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cloud.frizio.web.demospringrest.model.Book;
 import cloud.frizio.web.demospringrest.repository.BookRepository;;
 
 /**
@@ -13,4 +17,11 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+
+    // Find
+    @GetMapping("/books")
+    List<Book> findAll() {
+        return repository.findAll();
+    }
+    
 }
