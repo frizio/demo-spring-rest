440a8d480156f3aa3261c5f0d94bf97b8d29a253
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-18 14:49:20 +0200

    Save a book. (POST method)

--- src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
+++ src/main/java/cloud/frizio/web/demospringrest/controller/BookController.java
@@ -3,7 +3,11 @@ package cloud.frizio.web.demospringrest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import cloud.frizio.web.demospringrest.model.Book;
@@ -22,6 +26,13 @@ public class BookController {
     @GetMapping("/books")
     List<Book> findAll() {
         return repository.findAll();
+    }
+
+    // Save
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/books")
+    Book newBook(@RequestBody Book newBook) {
+        return repository.save(newBook);
     }
     
 }
