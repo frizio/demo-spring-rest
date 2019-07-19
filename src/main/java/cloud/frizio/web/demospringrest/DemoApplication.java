8ba1ca1397d0668e0e228a57906abb7514f619ac
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-19 15:01:40 +0200

    Add logger SLF4J

--- src/main/java/cloud/frizio/web/demospringrest/DemoApplication.java
+++ src/main/java/cloud/frizio/web/demospringrest/DemoApplication.java
@@ -2,6 +2,8 @@ package cloud.frizio.web.demospringrest;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,13 +15,17 @@ import cloud.frizio.web.demospringrest.repository.BookRepository;;
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
+        log.info("Applicazione in esecuzione");
 	}
 
 	@Bean
     CommandLineRunner initDatabase(BookRepository repository) {
         return args -> {
+            log.info("Inizializzazione database");
             repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
             repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
             repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
