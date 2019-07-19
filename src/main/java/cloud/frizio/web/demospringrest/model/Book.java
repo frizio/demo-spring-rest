3936389d3c449cce463c1a8ed9b71cb8d455ebdb
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-19 11:44:57 +0200

    Add bean validation constraint

--- src/main/java/cloud/frizio/web/demospringrest/model/Book.java
+++ src/main/java/cloud/frizio/web/demospringrest/model/Book.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,10 +24,14 @@ public class Book {
     @GeneratedValue
     private Long id;
     
+    @NotEmpty(message = "Please provide a name")
     private String name;
     
+    @NotEmpty(message = "Please provide a author")
     private String author;
     
+    @NotNull(message = "Please provide a price")
+    @DecimalMin("1.00")
     private BigDecimal price;
 
     public Book(String name, String author, BigDecimal price) {
